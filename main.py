6d729826b0511f25d275bba675858c5c71e1485b
DimaKalin1n <dimakalinin1207@gmail.com>
2023-09-26 19:39:15 +0300

    test

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print('Home page')
+from flask import Flask, render_template
+from redis import Redis
+
+app = Flask(__name__)
+
+r = Redis(host='127.0.0.1', port='6379')
+
+
+@app.route('/visit/<user_name>')
+def homePage(user_name):
+    r.incr(user_name)
+    return 'ok'
+
+
+@app.route('/visit/show')
+def show_page():
+    str1 = r.keys()
+    return str(len(str1))
+
+
+@app.route('/show/<user_name>')
+def show(user_name):
+    return r.get(user_name)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
