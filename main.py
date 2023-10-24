cc5bdc0d7acb5c0d6608f6d87b46ad9c455c1383
DimaKalin1n <dimakalinin1207@gmail.com>
2023-10-24 19:11:07 +0300

    finish

--- main.py
+++ main.py
@@ -15,18 +15,21 @@ r = Redis(host='redis', port=env['REDIS_PORT'])
 
 @app.route('/<user_name>')
 def home_page(user_name):
+    '''Добавлению пользвателя'''
     r.incr(user_name)
     return 'ok'
 
 
 @app.route('/visit/show')
 def show_page():
+    '''Показать кол-во юзеров'''
     str1 = r.keys()
     return str(len(str1))
 
 
 @app.route('/show/<user_name>')
 def show(user_name):
+    '''Порядковый номер'''
     return r.get(user_name)
 
 
